a1d2e9b7dcbb4a88b47ffaa05fd20a30bc759877
NergalIO <68157350+WokasWokas@users.noreply.github.com>
2022-10-12 23:10:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from voxel import image_to_pixel, Image
+from PIL import Image as img
 from camera import Camera
-from show import App
+from show import ImageApp
+from os import system
 
-def cycle(camera: Camera, app: App) -> None:
+def cycle(camera: Camera, app: ImageApp) -> None:
     frame = camera.get_image()
     if frame is None: return ''
     frame = Image(frame.tolist())
-    app.update_label(image_to_pixel(frame, 4))
+    app.update_label(image_to_pixel(frame, 1))
+
+def show_image(path: str, app: ImageApp) -> None:
+    image = img.open(path)
+    palette = [[image.getpixel((x, y)) for x in range(image.width)] for y in range(image.height)]
+    image = Image(palette)
+    image = image_to_pixel(image, 1)
+    print(image)
+    app.update_label(image)
 
 
 if __name__ == "__main__":
     cam = Camera()
 
-    app = App()
-    app.title("Camera 0")
+    app = ImageApp()
 
-    app.bind("<Button-2>", lambda event: app.loop_func(cycle, cam, app))
+    #app.title("Camera 0")
+    
+    #app.bind("<Button-2>", lambda event: app.loop_func(cycle, cam, app))
+
+    show_image("hyxWMpne_mc.jpg", app)
 
     app.start()
